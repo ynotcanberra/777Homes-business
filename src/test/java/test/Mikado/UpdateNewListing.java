0daf2a662e8e8c5b013b224a26340c0419a70b1a
ManojKarthick1992 <30752110+ManojKarthick1992@users.noreply.github.com>
2021-04-29 22:51:28 +0530

    Add files via upload

---
+++ src/test/java/test/Mikado/UpdateNewListing.java
@@ -0,0 +1,18 @@
+package test.Mikado;
+
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import AllHomesPageObjects.AllHomesPage;
+
+public class UpdateNewListing {
+
+	@Test
+	@SuppressWarnings("static-access")
+	public static void updateNewListing() throws IOException, InterruptedException {
+		WebDriver driver = null;
+		AllHomesPage allHomespage = new AllHomesPage(driver);
+		driver=allHomespage.updateNewListingInfo();
+		driver.quit();
+	}
+}
